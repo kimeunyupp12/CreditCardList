948ae99621a6c050a3fa79b516f3f570e413e6ae
eunyupp kim <kimeunyupp@gmail.com>
2022-03-24 01:37:15 +0900

    ui 구성 완료

---
+++ CreditCardList/CreditCard.swift
@@ -0,0 +1,27 @@
+//
+//  CreditCard.swift
+//  CreditCardList
+//
+//  Created by EunYupp Kim on 2022/03/24.
+//
+
+import Foundation
+
+struct CreditCard: Codable {
+    let id: Int
+    let rank: Int
+    let name: String
+    let cardImageURL: String
+    let promotionDetail: PromotionDetail
+    let isSelected: Bool?
+}
+
+struct PromotionDetail: Codable {
+    let companyName: String
+    let period: String
+    let amount: Int
+    let condition: String
+    let benefitCondition: String
+    let benefitDetail: String
+    let benefitDate: String
+}
